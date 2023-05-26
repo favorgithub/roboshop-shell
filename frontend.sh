515d7db340f11b9f9e9862d542623a11b8bcd030
favorgithub <gloify@yahoo.com>
2023-05-26 13:47:39 -0500

    Roboshop Nginx Script

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ echo -e "\e[33mRemoving Old App content\e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
 
 echo -e "\e[33mDownloading Frontend Contend\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip >/tmp/roboshop.log 2>/tmp/error.log
 
 
 echo -e "\e[33mExtracting Frontend Content\e[0m"
