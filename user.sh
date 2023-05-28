a463d7b01a1df8d2d6c1a066a0ec80a86daef459
favorgithub <gloify@yahoo.com>
2023-05-28 13:14:09 -0500

    roboshop nginx servers

--- user.sh
+++ user.sh
@@ -25,7 +25,7 @@ echo -e "\e[33m Install NodeJS Dependencies\e[0m"
 npm install &>>/tmp/roboshop.log
 
 echo -e "\e[33m Setup SystemD Service\e[0m"
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service  &>>/tmp/roboshop.log
 
 
 echo -e "\e[33m\e[0m"
