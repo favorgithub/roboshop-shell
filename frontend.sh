5b049fc586e09d1180c5637747b83483a422d257
favorgithub <gloify@yahoo.com>
2023-05-28 10:40:53 -0500

    roboshop nginx servers

--- frontend.sh
+++ frontend.sh
@@ -19,3 +19,6 @@ cp /home/centos/roboshop.conf/ /etc/nginx/default.d/roboshop.conf
 echo -e "\e[33mStarting Nginx Server\e[0m"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
+
+
+#
