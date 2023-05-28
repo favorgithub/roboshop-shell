b1a04feacc7341e355906ff97d62080a609edbab
favorgithub <gloify@yahoo.com>
2023-05-28 14:17:52 -0500

    roboshop nginx servers

--- cart.sh
+++ cart.sh
@@ -8,7 +8,7 @@ echo -e "\e[33m Add Application User\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[33m Create Application Directory\e[0m"
-rm -rf /app &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+rm -rf /app &>>/tmp/roboshop.log
 mkdir /app
 
 echo -e "\e[33m Download Application Content\e[0m"
