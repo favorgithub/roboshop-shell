16a70e9815e684b8951912537bd3d35a670af290
favorgithub <gloify@yahoo.com>
2023-05-28 17:07:15 -0500

    roboshop nginx servers

--- shipping.sh
+++ shipping.sh
@@ -16,7 +16,7 @@ cd /app
 unzip /tmp/shipping.zip &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download Maven Dependencies \e[0m"
-cd/app
+cd /app
 mvn clean package &>>/tmp/roboshop.log
 mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 
