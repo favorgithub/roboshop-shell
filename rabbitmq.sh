94b1b41508c7f21bb86a27fa8b0b3c40082f663d
favorgithub <gloify@yahoo.com>
2023-05-29 01:48:49 -0500

    roboshop nginx servers

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,16 +1,17 @@
-echo -e "\e[33m \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+echo -e "\e[33m Configure Erlang Repos \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+echo -e "\e[33m Configure Rabbitmq Repos \e[0m"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 
-echo -e "\e[33m Install Rabbitmq Server\e[0m"
-yum install rabbitmq-server -y
+echo -e "\e[33m Install RabbitMQ \e[0m"
+yum install rabbitmq-server -y &>>/tmp/roboshop.log
 
-echo -e "\e[33m \e[0m"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+echo -e "\e[33m Start RabbitMQ Service \e[0m"
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl restart rabbitmq-server &>>/tmp/roboshop.log
 
-echo -e "\e[33m Start Service \e[0m"
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+echo -e "\e[33m Add Rabbitmq Application userApplication \e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+
