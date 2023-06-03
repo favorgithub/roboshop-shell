f02cc422a4d6c1f96649a08c7fcb2d34fbc72588
favorgithub <gloify@yahoo.com>
2023-06-02 19:35:30 -0500

    roboshop nginx servers

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,17 +1,25 @@
-echo -e "\e[33m Configure Erlang Repos \e[0m"
+source common.sh
+
+echo -e "\${color} Configure Erlang Repos ${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+stat_check $?
 
-echo -e "\e[33m Configure Rabbitmq Repos \e[0m"
+echo -e "\${color} Configure Rabbitmq Repos ${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+stat_check $?
 
-echo -e "\e[33m Install RabbitMQ \e[0m"
+echo -e "\${color} Install RabbitMQ ${nocolor}"
 yum install rabbitmq-server -y &>>/tmp/roboshop.log
+stat_check $?
 
-echo -e "\e[33m Start RabbitMQ Service \e[0m"
+
+echo -e "\${color} Start RabbitMQ Service ${nocolor}"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.log
 systemctl restart rabbitmq-server &>>/tmp/roboshop.log
+stat_check $?
 
-echo -e "\e[33m Add Rabbitmq Application User \e[0m"
+echo -e "\${color} Add Rabbitmq Application User ${nocolor}"
 rabbitmqctl add_user roboshop $1 &>>/tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+stat_check $?
 
